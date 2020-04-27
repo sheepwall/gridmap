24500d0ce63747bf7e1a5a3fd03cb8fb35eb0924
sheepwall <a.sve@live.se>
2020-04-27 14:14:58 +0200

    Basic perlin noise map gen + ray casting

---
+++ Gridmap.py
@@ -0,0 +1,168 @@
+import noise
+from matplotlib import pyplot as plt
+import numpy as np
+
+class Gridmap:
+    def __init__(
+        self,
+        xlim,
+        ylim,
+        occupancy_grid
+    ):
+        self.xlim = xlim
+        self.ylim = ylim
+
+        # Number of squares
+        self.M = np.size(occupancy_grid, axis=0)
+        self.N = np.size(occupancy_grid, axis=1)
+
+        self.xres = (xlim[1] - xlim[0]) / self.M
+        self.yres = (ylim[1] - ylim[0]) / self.N
+
+        self.xaxis = np.linspace(xlim[0], xlim[1], self.M + 1)
+        self.yaxis = np.linspace(ylim[0], ylim[1], self.N + 1)
+
+        self.og = occupancy_grid
+
+    def plot(self, plot_axes=None):
+        if plot_axes is None:
+            fig = plt.figure()
+            plot_axes = fig.add_subplot(111)
+
+        plot_axes.pcolormesh(
+            self.xaxis,
+            self.yaxis,
+            self.og.T,
+            vmin=0,
+            vmax=1,
+            cmap='gray_r',
+            linewidths=0.1,
+            edgecolor='k'
+        )
+
+    def value_at(self, position):
+        hits = np.where(position[0] > self.xaxis)[0]
+        i = max(hits)
+
+        hits = np.where(position[1] > self.yaxis)[0]
+        j = max(hits)
+
+        return self.og[i,j]
+
+    def clear_view(self, p1, p2):
+        a = (p2[1] - p1[1])/(p2[0] - p1[0])
+        b = p1[1] - p1[0] * a
+
+        line = lambda x: a * x + b
+        invline = lambda y: (y - b) / a
+
+        ax = plt.gca()
+        ax.plot(p1[0], p1[1], 'ro')
+        ax.plot(p2[0], p2[1], 'bo')
+
+        xl = np.linspace(p1[0], p2[0], 3)
+        yl = line(xl)
+        ax.plot(xl, yl)
+
+        xmin = min([p1[0], p2[0]])
+        xmax = max([p1[0], p2[0]])
+        ymin = min([p1[1], p2[1]])
+        ymax = max([p1[1], p2[1]])
+
+        xintersects = self.xaxis[(self.xaxis > xmin) & (self.xaxis < xmax)]
+        xintersects = np.c_[xintersects, line(xintersects)]
+        yintersects = self.yaxis[(self.yaxis > ymin) & (self.yaxis < ymax)]
+        yintersects = np.c_[invline(yintersects), yintersects]
+
+
+        for p in xintersects:
+            dx = np.array([self.xres / 2, 0])
+            square_r = self.value_at(p + dx)
+            square_l = self.value_at(p - dx)
+            if square_r == 1 or square_l == 1:
+                return False
+
+        for p in yintersects:
+            dy = np.array([0, self.yres / 2])
+            square_u = self.value_at(p + dy)
+            square_d = self.value_at(p - dy)
+            if square_u == 1 or square_d == 1:
+                return False
+
+        return True
+
+    @classmethod
+    def from_perlin_noise(
+            cls,
+            xlim,
+            ylim,
+            M,
+            N,
+            seed=4,
+            scale=40,
+            threshold=0.7
+    ):
+
+        pmap = cls.perlin_map(xlim, ylim, M, N, seed, scale)
+
+        # Make it binary
+        maximum = np.max(pmap)
+        minimum = np.min(pmap)
+        threshold = minimum + threshold * (maximum - minimum)
+
+        pmap = (pmap > threshold).choose(pmap, 1)
+        pmap = (pmap <= threshold).choose(pmap, 0)
+
+        return cls(
+            xlim=xlim,
+            ylim=ylim,
+            occupancy_grid=pmap
+        )
+
+    @staticmethod
+    def perlin_map(xlim, ylim, M, N, seed=None, scale=40):
+        if seed is None:
+            seed = np.random.randint(0, 200)
+
+        xres = (xlim[1] - xlim[0]) / M
+        yres = (ylim[1] - ylim[0]) / N
+
+        xaxis = np.linspace(xlim[0], xlim[1], M + 1)
+        yaxis = np.linspace(ylim[0], ylim[1], N + 1)
+
+        perlin_map = np.zeros((M, N))
+
+        for i, x in enumerate(perlin_map):
+            for j, y in enumerate(perlin_map[i]):
+                perlin_map[i,j] = noise.pnoise2(
+                    (xaxis[i] + xres/2) / scale,
+                    (yaxis[j] + yres/2) / scale,
+                    base=seed
+                )
+
+        return perlin_map
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    gm = Gridmap.from_perlin_noise(
+        xlim=np.array([-50, 50]),
+        ylim=np.array([-50, 50]),
+        M=100,
+        N=100,
+        seed=6,
+        scale=30,
+        threshold=0.9
+    )
+
+    gm.plot(plot_axes=ax)
+
+    p1 = 100 * np.random.rand(2) - 50
+    p2 = 100 * np.random.rand(2) - 50
+
+    clr = gm.clear_view(p1, p2)
+    print(clr)
+
+    plt.show()
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A pack to create a 2.5D map that can do ray casting/shooting.
 ### Map creation
 
 **Generate** a map, with a few parameters to determine cluster size,
-prevalence...
+prevalence... DONE
 
 **Import** a map from some file format. Should be easy to use with a height map,
 e.g. from satellite images.
@@ -14,5 +14,5 @@ e.g. from satellite images.
 ### Ray casting/shooting
 
 **Check obstruction** by supplying two positions on the map and return a boolean
-true or false if an obstacle was hit.
+true or false if an obstacle was hit. DONE
 
